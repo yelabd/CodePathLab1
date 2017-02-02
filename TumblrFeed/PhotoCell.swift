50e17dabe2e58131117d80249d6bdab02896c4a0
Youssef <yelabd@purdue.edu>
2017-02-02 18:51:43 -0500

    half of milestone 3

---
+++ TumblrFeed/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  TumblrFeed
+//
+//  Created by Youssef Elabd on 2/2/17.
+//  Copyright © 2017 Youssef Elabd. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
