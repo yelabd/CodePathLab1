522047011f4a1e85fac79a5ea7df8199318a4507
Youssef <yelabd@purdue.edu>
2017-02-09 19:59:36 -0500

    Added infinite scrolling

--- TumblrFeed/PhotosViewController.swift
+++ TumblrFeed/PhotosViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 import AFNetworking
 
-class PhotosViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+class PhotosViewController: UIViewController,UITableViewDataSource,UITableViewDelegate,UIScrollViewDelegate {
 
     var posts: [NSDictionary] = []
-    
+    var isMoreDataLoading = false
+    var incrementor : Int = 0
+    let refreshControl = UIRefreshControl()
+
     
     @IBOutlet weak var feedTableView: UITableView!
     
@@ -21,8 +24,7 @@ class PhotosViewController: UIViewController,UITableViewDataSource,UITableViewDe
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: #selector(refreshControlAction(refreshControl:)), for: UIControlEvents.valueChanged)
+                refreshControl.addTarget(self, action: #selector(refreshControlAction(refreshControl:)), for: UIControlEvents.valueChanged)
         feedTableView.insertSubview(refreshControl, at: 0)
         feedTableView.rowHeight = 240
         // Do any additional setup after loading the view.
@@ -141,6 +143,50 @@ class PhotosViewController: UIViewController,UITableViewDataSource,UITableViewDe
         });
         task.resume()
 
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        // Handle scroll behavior here
+        let scrollViewContentHeight = feedTableView.contentSize.height
+        let scrollOffsetThreshold = scrollViewContentHeight - feedTableView.bounds.size.height
+        
+        // When the user has scrolled past the threshold, start requesting
+        if(scrollView.contentOffset.y > scrollOffsetThreshold && feedTableView.isDragging) {
+            isMoreDataLoading = true
+            incrementor = incrementor + 20
+            
+            let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV&offset=\(incrementor)")
+            let request = URLRequest(url: url!)
+            let session = URLSession(
+                configuration: URLSessionConfiguration.default,
+                delegate:nil,
+                delegateQueue:OperationQueue.main
+            )
+            
+            let task : URLSessionDataTask = session.dataTask(
+                with: request as URLRequest,
+                completionHandler: { (data, response, error) in
+                    if let data = data {
+                        if let responseDictionary = try! JSONSerialization.jsonObject(
+                            with: data, options:[]) as? NSDictionary {
+                            print("responseDictionary: \(responseDictionary)")
+                            
+                            // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                            // This is how we get the 'response' field
+                            let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+                            
+                            // This is where you will store the returned array of posts in your posts property
+                            //                        self.feeds = responseFieldDictionary["posts"] as! [NSDictionary]
+                            self.posts = self.posts + (responseFieldDictionary["posts"] as! [NSDictionary])
+                            self.feedTableView.reloadData()
+                            self.refreshControl.endRefreshing()
+                        }
+                    }
+            });
+            task.resume()
+
+            // ... Code to load more results ...
+        }
     }
 
     
